d98ed9ae05eeb568836be135accdebacd446e6a7
Milton Montero <lleramontero@gmail.com>
2026-07-29 01:30:52 +0200

    Add utility script to extract shape from stock image.

--- mindset/shapes/__init__.py
+++ mindset/shapes/__init__.py
@@ -1,5 +1,6 @@
 """mindset shape base classes and utilities."""
 
 from mindset.shapes.manipulation import BaseDrawManipulatedObject
+from mindset.shapes.cropping import SegmentationResult, ShapeExtractor
 
-__all__ = ["BaseDrawManipulatedObject"]
+__all__ = ["ShapeExtractor", "SegmentationResult", "BaseDrawManipulatedObject"]
