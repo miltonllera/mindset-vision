3f17c970e747156f57a8fc210d614155cc001188
Milton Montero <lleramontero@gmail.com>
2026-09-03 22:19:02 +0200

    Bump Numpy and Matplotlib versions.

--- mindset/drawing/shapes/utils/color.py
+++ mindset/drawing/shapes/utils/color.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 
 def get_rgba_color(num):
@@ -6,7 +6,7 @@ def get_rgba_color(num):
     Returns a unique RGBA color tuple for a given number less than 10.
     """
     assert num >= 0 and num < 10, "Number should be between 0 and 9"
-    colors = plt.cm.get_cmap("tab10", 10)  # get a color map with 10 colors
+    colors = mpl.colormaps["tab10"]
     rgba = colors(num)  # get the RGBA color tuple for the given number
     rgba = tuple([int(255 * x) for x in rgba])
     return rgba
